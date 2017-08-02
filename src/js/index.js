46b6b2f2b198ad10afcd139ef80784ebf6697b44
Eneymotorny <eneymotorny@gmail.com>
2017-08-02 08:45:00 +0300

    full functionality pencil; styles, small additional features

--- src/js/index.js
+++ src/js/index.js
@@ -10,55 +10,157 @@ class CanvasLine {
 		this.canvas = this.elem[0];
 		this.ctx = this.canvas.getContext('2d');
 		this.panelTool = new PanelTool(this);
-		this.drawPen()
+
+		this.draw('pencil');
+		this.addCursor()
 	}
-	drawPen() {
-		this.elem.off();
+	addCursor() {
+		let cursor = $('<div class="cursor"></div>')
+			.width( this.panelTool.getWidth() )
+			.height( this.panelTool.getWidth() )
+			.insertBefore(this.elem);
+		this.elem.on('mousemove', (e) => pozitCursor(e) );
+
+		this.panelTool.colrpcr.on('change', () => {
+			cursor.css('border-color', this.panelTool.getColor() )
+		});
+
+		function pozitCursor(e) {
+			let
+				w = cursor.width()/2,
+				h = cursor.height()/2,
+				y = (e.offsetY - w - 2),
+				x = (e.offsetX - h - 2);
+			cursor.css('transform', `translateY(${y}px) translateX(${x}px)`);
+		}
+		this.elem.on('mouseout', () => cursor.hide());
+		this.elem.on('mouseover', () => cursor.show());
+
+		this.elem.on('wheel', (e) => {
+			e.preventDefault();
+
+			if (e.originalEvent.deltaY < 0) {
+				this.panelTool.plusWidth();
+				cursor
+					.width( this.panelTool.getWidth() )
+					.height( this.panelTool.getWidth() )
+			} else {
+				this.panelTool.minusWidth();
+				cursor
+					.width( this.panelTool.getWidth() )
+					.height( this.panelTool.getWidth() )
+			}
+			pozitCursor(e);
+		});
+	}
+	draw(tool) {
+		this.elem.off('.draw');
 		let
-			width = this.panelTool.width(),
-			color = this.panelTool.color(),
 			drawing = false,
 			ctx = this.ctx;
+		ctx.lineWidth = this.panelTool.getWidth();
+		ctx.strokeStyle = this.panelTool.getColor();
+		ctx.lineCap = 'round';
 
-		this.elem.on('mousedown', function (e) {
-			drawing = true;
-			ctx.beginPath();
-			ctx.moveTo(e.offsetX, e.offsetY);
-			ctx.lineWidth = width;
-			ctx.lineCap = 'round';
-			ctx.strokeStyle = '#' + color;
-			ctx.stroke();
-		});
-		this.elem.on('mousemove', function (e) {
-			if (drawing) {
+		if (tool === 'pencil') {
+			this.elem.on('mousedown.draw', function (e) {
+				drawing = true;
+				ctx.beginPath();
+				ctx.moveTo(e.offsetX, e.offsetY);
+				ctx.stroke();
+			});
+			this.elem.on('mousemove.draw', function (e) {
+				if (drawing) {
+					ctx.lineTo(e.offsetX, e.offsetY);
+					ctx.stroke();
+				}
+			});
+			this.elem.on('mouseup.draw', function (e) {
 				ctx.lineTo(e.offsetX, e.offsetY);
 				ctx.stroke();
-			}
-		});
-		this.elem.on('mouseup', () => { drawing = false } );
-		this.elem.on('mouseout', (e) => e.buttons === 1 ? ctx.closePath(): drawing = false );
-		this.elem.on('mouseover', (e) => e.buttons !== 1 ? drawing = false : ctx.beginPath() );
+				drawing = false
+			});
+			this.elem.on('mouseout.draw', (e) => (e.buttons === 1) ? ctx.closePath() : drawing = false );
+			this.elem.on('mouseover.draw', (e) => (e.buttons !== 1) ? drawing = false : ctx.beginPath() );
+		} else
+
+		if (tool === 'line') {
+			let x, y;
+			this.elem.on('mousedown.draw', function (e) {
+				drawing = true;
+				ctx.beginPath();
+				x = e.offsetX;
+				y = e.offsety;
+				ctx.moveTo(x, y);
+
+			});
+			this.elem.on('mousemove.draw', function (e) {
+				if (drawing) {
+					ctx.moveTo(x, y);
+					ctx.lineTo(e.offsetX, e.offsetY);
+					ctx.stroke();
+					ctx.closePath();
+				}
+			});
+			this.elem.on('mouseup.draw', function (e) {
+				if (drawing) {
+					ctx.lineTo(e.offsetX, e.offsetY);
+					ctx.stroke();
+					ctx.closePath();
+					drawing = false
+				}
+			});
+			/*this.elem.on('mouseup', () => drawing = false );*/
+			this.elem.on('mouseout.draw', () => drawing = false );
+		} // expected
 	}
 	clearCanvas(){
 		this.ctx.clearRect(0, 0, this.canvas.width, this.canvas.height)
 	}
 }
 class PanelTool {
-	constructor (canvasLine) {
+	constructor (canvas) {
+		this.canvas = canvas;
 		let $tools =
-			$('<div class="panel-tools">').insertBefore(canvasLine.elem);
-		let $colrpcr =
-			$('<input class="jscolor" value="00000">').appendTo($tools);
-		let $inpWidth =
+			$('<div class="panel-tools">').insertBefore(canvas.elem);
+
+		this.colrpcr =
+			$(`<input class="jscolor {
+				width: 123,
+				padding: 0,
+				shadow: false,
+				borderWidth: 0,
+				backgroundColor: 'transparent',
+				insetColor: '#fff',
+				position: 'top',
+				hash: true
+			}"
+			value="#000000">`).appendTo($tools);
+		$('<span>').text('Color').insertBefore(this.colrpcr);
+
+		this.inpWidth =
 			$('<input type="number" min="1" max="70" value="6">').appendTo($tools);
 			$('<button>Clear</button>').appendTo($tools)
-				.on('click', () => canvasLine.clearCanvas() );
+				.on('click', () => canvas.clearCanvas() );
 
-		$tools.children().on('change', () => canvasLine.drawPen() );
+		$tools.children().on('change', () => this.refresh() );
+		$('<span>').text('Width').insertBefore(this.inpWidth);
 
-		this.color = function() { return $colrpcr.val() };
-		this.width = function() { return $inpWidth.val() }
 	}
+	refresh() { this.canvas.draw('pencil') }
+	getColor () { return this.colrpcr.val() };
+	getWidth () { return this.inpWidth.val() };
+	plusWidth () {
+		if (this.inpWidth.val() < 70 )
+		this.inpWidth.val( +this.inpWidth.val() + 1 );
+		this.refresh()
+	};
+	minusWidth () {
+		if (this.inpWidth.val() > 1 )
+		this.inpWidth.val( +this.inpWidth.val() - 1 );
+		this.refresh()
+	};
 }
 
 let newCanvas = new CanvasLine('drawLine');
+console.dir(newCanvas);
